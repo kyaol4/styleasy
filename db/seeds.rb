2a16507937c339bfa576b6e28c2ba9f16e437dc9
dongwook kim <kyaol4@gmail.com>
2022-02-28 17:29:02 +0900

    made user seeds and set git ignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users
+User.destroy_all
+puts "Creating all the users......"
+User.create!(
+  email: "tobymanley1990@gmail.com",
+  password: "password",
+  name: "Toby",
+  gender: "Male",
+  top_size: "M",
+  bottom_size: "M"
+)
+User.create!(
+  email: "saorimita4@gmail.com",
+  password: "password",
+  name: "Saori",
+  gender: "Female",
+  top_size: "M",
+  bottom_size: "M"
+)
+User.create!(
+  email: "r.winstonc@gmail.com",
+  password: "password",
+  name: "Winston",
+  gender: "Male",
+  top_size: "M",
+  bottom_size: "M"
+)
+User.create!(
+  email: "kyaol4@gmail.com",
+  password: "password",
+  name: "Dongwook",
+  gender: "Male",
+  top_size: "M",
+  bottom_size: "M"
+)
