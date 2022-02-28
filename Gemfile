5a38012c75d19791fb00710ebcc787083638d0bd
dongwook kim <kyaol4@gmail.com>
2022-02-28 17:36:07 +0900

    set active storage, set cloudinary

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'devise'
 
+# custom
+gem 'cloudinary', '~> 1.16.0'
+
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
 gem 'simple_form'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :shop
 
+  has_one_attached :photo
+
   validates :size, presence: true
   validates :price, presence: true
   validates :brand, presence: true
