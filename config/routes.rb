3481c1c95f0a371e888f4e668b891ae66c29e4e7
Toby Manley <tobymanley1990@gmail.com>
2022-03-03 10:27:58 +0900

    commented out stuff in routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root :to => 'home#index'
-  get '/products', :to => 'products#index'
-  mount ShopifyApp::Engine, at: '/'
+  # root :to => 'home#index'  #<<< SHOPIFY
+  # get '/products', :to => 'products#index'  #<<<<  SHOPIFY
+  # mount ShopifyApp::Engine, at: '/'  #<<<<< SHOPIFY
   devise_for :users
-  # root to: 'pages#home'
+  root to: 'pages#home' #<<<< LOCAL
   resources :items, only: [:show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
