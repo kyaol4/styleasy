1c9f9e839c543c5b352996cdde7cebf09ec61141
Winston Chu <r.winstonc@gmail.com>
2022-03-08 13:27:40 +0900

    first landing pg

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root :to => 'home#index'  #<<< SHOPIFY
-  get '/products', :to => 'products#index'  #<<<<  SHOPIFY
-  mount ShopifyApp::Engine, at: '/'  #<<<<< SHOPIFY
+  # root :to => 'home#index'  #<<< SHOPIFY
+  # get '/products', :to => 'products#index'  #<<<<  SHOPIFY
+  # mount ShopifyApp::Engine, at: '/'  #<<<<< SHOPIFY
   devise_for :users
-  # root to: 'pages#home' #<<<< LOCAL
+  root to: 'pages#home' #<<<< LOCAL
   resources :items, only: [:show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
