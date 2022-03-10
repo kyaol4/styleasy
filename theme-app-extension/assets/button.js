f16a0b0e7b9441bc94fd2082936d3d2dc34e521b
dongwook kim <kyaol4@gmail.com>
2022-03-10 18:32:01 +0900

    modified btn and show explain how to use camera gesture

--- theme-app-extension/assets/button.js
+++ theme-app-extension/assets/button.js
@@ -1,13 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
    var button = document.getElementById("ARbutton");
    var x = document.getElementById("AR");
+   var ep = document.getElementById("explain")
    console.log(button, x)
    button.addEventListener('click', (event)=>{
      console.log('check')
       if (x.style.display === "none") {
       x.style.display = "block";
+      ep.style.display = "block";
     } else {
       x.style.display = "none";
+      ep.style.display = "none";
     }
-                      })
-   });
+  })
+});
